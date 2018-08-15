b16690951952e7c57c682f9d3b808546fab2e4cd
Mayo <Erica4fr@zoho.com>
2018-08-15 00:21:53 -0500

    Ejerecicio Mi primera calculadora

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+numero_1 = (input("Primer numer: "))
+numero_2 = (input("Segundo numero: "))
+
+opcion = input("Que decea hacer, Suma = a Resta = b Multiplicacion = c Divicion = d")
+
+if opcion == "a"
+    suma = numero_1 + numero_2
+    print("El resultado es: ",suma)
+
+if opcion == "b"
+    resta = numero_1 - numero_2
+    print("El resultado es: ",resta)
+
+if opcion == "c"
+    multiplicacion = numero_1 * numero_2
+    print("El resultado es:",multiplicacion)
+
+if opcion == "d"
+    divicion = numero_1 % numero_2
+    print("El resultado es",divicion)
+
